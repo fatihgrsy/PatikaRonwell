8baea970673d5852468ebcce591ea1f3de7740e6
Fatih Gürsoy <fatihgrsy@hotmail.com>
2022-02-25 18:24:19 +0300

    EfApi bitti

---
+++ RonvelEFApi/Controllers/ClassRoomController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace RonvelEFApi.Controllers
+{
+    public class ClassRoomController : ApiController
+    {
+
+        Models.SchoolContext _context;
+        public ClassRoomController()
+        {
+            if (_context == null)
+                _context = new Models.SchoolContext();
+        }
+
+        [HttpPost]
+        [Route("ClassRoomRange")]
+        public IHttpActionResult ClassRoomRange([FromBody] List<Models.ClassRoom> model)
+        {
+            try
+            {
+                _context.ClassRooms.AddRange(model);
+                _context.SaveChanges();
+                return Ok("Başarılı");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Hatasız kul olmaz. Hatamla sev beni.");
+            }
+        }
+        [HttpPost]
+        [Route("ClassRoomAdd")]
+        public IHttpActionResult ClassRoomAdd([FromBody] Models.ClassRoom model)
+        {
+            try
+            {
+                _context.ClassRooms.Add(model);
+                _context.SaveChanges();
+                return Ok("Başarılı");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Hatasız kul olmaz. Hatamla sev beni.");
+            }
+        }
+
+        [HttpPut]
+        [Route("ClassRoomUpdate")]
+        public IHttpActionResult ClassRoomUpdate([FromBody] Models.ClassRoom model)
+        {
+            try
+            {
+                var data = _context.ClassRooms.FirstOrDefault(x => x.Id == model.Id);
+                data.ClassRoomName = model.ClassRoomName;
+                data.SchoolId = model.SchoolId;
+                data.Id = model.Id;
+                _context.SaveChanges();
+                return Ok("Başarılı");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Hatasız kul olmaz. Hatamla sev beni.");
+            }
+        }
+
+        [HttpDelete]
+        [Route("ClassRoomRemove")]
+        public IHttpActionResult ClassRoomRemove([FromBody] int id)
+        {
+            try
+            {
+                var data = _context.ClassRooms.FirstOrDefault(x => x.Id == id);
+                _context.ClassRooms.Remove(data);
+                _context.SaveChanges();
+                return Ok("Başarılı");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Hatasız kul olmaz. Hatamla sev beni.");
+            }
+        }
+
+        [HttpGet]
+        [Route("GetClassRoomIds")]
+        public IHttpActionResult GetClassRoomIds([FromUri] int id)
+        {
+            try
+            {
+                return Ok(_context.ClassRooms.FirstOrDefault(x => x.Id == id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Hatasız kul olmaz. Hatamla sev beni.");
+            }
+        }
+
+        [HttpGet]
+        [Route("GetClassRoomAll")]
+        public IHttpActionResult GetClassRoomAll()
+        {
+            try
+            {
+                return Ok(_context.ClassRooms.ToList());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Hatasız kul olmaz. Hatamla sev beni.");
+            }
+        }
+    }
+}
---
+++ RonvelEFApi/Controllers/StudentController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace RonvelEFApi.Controllers
+{
+    public class StudentController : ApiController
+    {
+        Models.SchoolContext _context;
+        public StudentController()
+        {
+            if (_context == null)
+                _context = new Models.SchoolContext();
+        }
+
+        [HttpPost]
+        [Route("StudentAddRange")]
+        public IHttpActionResult StudentAddRange([FromBody] List<Models.Student> model)
+        {
+            try
+            {
+                _context.Students.AddRange(model);
+                _context.SaveChanges();
+                return Ok("Başarılı");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Hatasız kul olmaz. Hatamla sev beni.");
+            }
+        }
+
+        [HttpPost]
+        [Route("StudentAdd")]
+        public IHttpActionResult StudentAdd([FromBody] Models.Student model)
+        {
+            try
+            {
+                _context.Students.Add(model);
+                _context.SaveChanges();
+                return Ok("Başarılı");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Hatasız kul olmaz. Hatamla sev beni.");
+            }
+        }
+
+        [HttpPut]
+        [Route("StudentUpdate")]
+        public IHttpActionResult StudentUpdate([FromBody] Models.Student model)
+        {
+            try
+            {
+                var data = _context.Students.FirstOrDefault(x => x.Id == model.Id);
+                if (data != null)
+                {
+                    data.StudentNameSurname = model.StudentNameSurname;
+                    data.ClassRoomId = model.ClassRoomId;
+                    _context.SaveChanges();
+                    return Ok("Başarılı");
+                }
+                else
+                    return Ok("null");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Hatasız kul olmaz. Hatamla sev beni.");
+            }
+        }
+
+
+
+        [HttpDelete]
+        [Route("StudentRemove")]
+        public IHttpActionResult StudentRemove([FromBody] int Id)
+        {
+            try
+            {
+                var data = _context.Students.FirstOrDefault(x => x.Id == Id);
+                _context.Students.Remove(data);
+                _context.SaveChanges();
+                return Ok("Başarılı");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Hatasız kul olmaz. Hatamla sev beni.");
+            }
+        }
+
+        [HttpGet]
+        [Route("GetStudentIds")]
+        public IHttpActionResult GetStudentIds([FromUri] int id)
+        {
+            try
+            {
+                return Ok(_context.Students.FirstOrDefault(x => x.Id == id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Hatasız kul olmaz. Hatamla sev beni.");
+            }
+        }
+
+        [HttpGet]
+        [Route("GetStudentAll")]
+        public IHttpActionResult GetStudentAll()
+        {
+            try
+            {
+                return Ok(_context.Students.ToList());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Hatasız kul olmaz. Hatamla sev beni.");
+            }
+        }
+    }
+}
